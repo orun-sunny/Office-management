f87cbc453be8d3f4fb24ad6def85eaf98f1fcdd3
orun-sunny~ <sunny.mukut@gmail.com>
2023-03-01 19:22:03 +0600

    providnetProfiel

--- src/components/Demo/Demo.js
+++ src/components/Demo/Demo.js
@@ -1,27 +1,67 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import pro from "../../components/images/pro.png"
 
 function Demo() {
-    const [demos,setDemos] =useState([]);
-    // fetch('https://polls.apiblueprint.org/api/department')
-    // TouchEvent(res =>res.json())
-    // .then(data =>{})
-// console.log(demos)
-    useEffect (()=>{
-        axios.get('https://private-155d5-officemanager.apiary-mock.com/api/department')
-        .then(data =>{
-            const demos = data.data.data[0].sub_departments;
-            console.log(demos)
-            // setDemos(demos)
-            // const departmentLoaded = demos.map(demo=>{
-            
 
-            // })
-        });
-
-    },[])
   return (
-    <div>Demo</div>
+    <>
+      <div className="w-full  bg-gray-200  dark:bg-gray-800   flex  items-center  justify-center  ">
+        <div className="container lg:w-full  sm:w-full md:w-full    bg-white  shadow-lg    transform   duration-200 easy-in-out">
+          <div className=" h-32 overflow-hidden" >
+            <img className="w-full" src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80" alt="" />
+          </div>
+          <div className="flex  px-5  -mt-12">
+            <img className="h-32 w-32 bg-white p-2 rounded-full   " src={pro} alt="" />
+
+          </div>
+          <div className="h-full ">
+            <div className=" px-4">
+              <h2 className="text-gray-800 text-3xl font-bold">Amazing</h2>
+              <a className="text-gray-400 mt-2 hover:text-blue-500" href="https://www.instagram.com" target="BLANK()">i am habib</a>
+              <p className="mt-2 text-gray-500 text-sm">email:here</p>
+              <p><span className="font-semibold"> phone: </span> Facebook</p>
+              <p><span className="font-semibold"> office-id: </span> Facebook</p>
+            </div>
+            <hr className="mt-6" />
+
+          </div>
+        </div>
+      </div>
+
+
+      <hr />
+
+
+      <div className="flex items-center justify-center pb-4">
+        <div className="bg-gray-200  w-full mt-10 rounded-lg">
+          <div className="flex items-center justify-center pt-10 flex-col">
+            <img src={pro} className="rounded-full w-32" />
+            <h1 className="text-gray-800 font-semibold text-xl mt-5">Habib Bhaiii</h1>
+            {/* <h1 className="text-gray-500 text-sm">Seoul, South Korea</h> */}
+            <h1 className="text-gray-500 text-sm p-4 text-center">
+            office-id:12345
+            </h1>
+            <h1 className="text-gray-500 text-sm p-4 text-center">
+            Phone:12345
+            </h1>
+          </div>
+          <div className="flex justify-between p-4">
+         
+            {/* <div>
+              <button className="text-xs text-green-300 border-2 py-1 px-2 border-green-300">Renew</button>
+            </div> */}
+          </div>
+          <div className="flex items-center justify-center mt-3 mb-8 flex-col">
+            <h1 className="text-xs text-gray-500">Get Connected</h1>
+
+          </div>
+        </div>
+
+      </div>
+
+
+    </>
   )
 }
 
--- src/components/ProvidentProfile/ProvidentProfile.js
+++ src/components/ProvidentProfile/ProvidentProfile.js
@@ -1,6 +1,7 @@
 
 import { Controller, useForm } from 'react-hook-form';
 import React, { useState } from "react";
+import pro from "../../components/images/pro.png"
 
 
 function ProvidentProfile() {
@@ -19,131 +20,41 @@ function ProvidentProfile() {
     const onSubmit = (data) => console.log(data);
     return (
 
-        <div className="w-full p-4 sm:w-full md:w-full lg:w-full justify-center" >
+        <div>
+      <div className="w-full  bg-gray-200  dark:bg-gray-800   flex  items-center  justify-center  ">
+            <div className=" lg:w-full  sm:w-full md:w-full    bg-white  shadow-lg    transform   duration-200 easy-in-out">
+                <div className=" h-32 " >
+                
+                </div>
+                <div className="flex  px-5  -mt-12">
+                    <img className="h-32 w-32 bg-white p-2 rounded-full   " src={pro} alt="" />
 
-
-
-            <h4 className='font-bold text-center'>  Provident Profile</h4>
-
-
-
-
-            <div className=" w-28 h-28 mb-2">
-                <img className="w-full h-full " src="https://png.pngtree.com/png-clipart/20201208/original/pngtree-red-tilted-double-love-heart-clipart-png-image_5565492.jpg" />
-
-
-            </div>
-
-            <div className="w-full h-9 bg-gray-100 mt-4 bottom-0 right-2  justify-center items-center">
-                <p>Hiiiiiii use me as a name</p>
-
-            </div>
-
-            <div className='w-full bg-gray-100 rounded-full mt-4  bottom-0 right-2 '>
-                <p>Phone Number</p>
-
-            </div>
-
-            <div className='w-full bg-gray-100 rounded-full mt-4  bottom-0 right-2 '>
-                <p>Office Id</p>
-
-            </div>
-
-
-            {/* Extra */}
-
-
-
-            {/* <div className="flex flex-col lg:flex-row lg:items-center lg:gap-5 w-full mb-3">
-
-                <div>
-                    <div className="flex">
-                        <div className="flex items-center justify-center border rounded-l-md px-3 dark:text-black">
-                            <select>
-                                <option value="one">+88</option>
-                            </select>
-                        </div>
-                        <div
-
-                            className="w-full border py-3 pl-3 rounded-r-md p-2 focus:outline-none"
-                            type="number"
-                            placeholder="Contact Number"
-                            {...register("contact_number", {
-                                required: true,
-                                minLength: 8,
-                                maxLength: 11,
-                            })}
-                        />
+                </div>
+                <div className=" min-h-full">
+                    <div className=" px-8 pb-4">
+                        <h2 className="text-gray-800 text-3xl font-bold">Amazing</h2>
+                        <a className="text-gray-400 mt-2 hover:text-blue-500" href="https://www.instagram.com" target="BLANK()">i am habib</a>
+                        <p className="mt-4 text-gray-500 text-sm pb-4">email:here</p>
+                        <p><span className="font-semibold pb-4"> phone: </span> Facebook</p>
+                        <p><span className="font-semibold pb-4"> office-id: </span> Facebook</p>
+                        <p><span className="font-semibold pb-4"> phone: </span> Facebook</p>
+                        <p><span className="font-semibold"> office-id: </span> Facebook</p>
                     </div>
-                    {errors?.contact_number?.type === "required" && (
-                        <p className="text-red-500">This field is required</p>
-                    )}
+                    {/* <hr className="mt-6" /> */}
+                    {/* <div className="flex  bg-gray-50 ">
+                        <div className="text-center w-1/2 p-4 hover:bg-gray-100 cursor-pointer">
+                            <p><span className="font-semibold"> phone: </span> Facebook</p>
+                        </div>
+                        <div className="border"></div>
+                        <div className="text-center w-1/2 p-4 hover:bg-gray-100 cursor-pointer">
+                            <p> <span className="font-semibold">office_id:</span> Linkedin</p>
+                        </div>
+
+                    </div> */}
                 </div>
-
-                <div>
-                    <input
-                        className="border py-3 w-48 pl-3 rounded-md focus:outline-none"
-                        type="number"
-                        min={1}
-                        defaultValue={1}
-                        placeholder="Project Count"
-                        {...register(
-                            "project_count",
-
-                            {
-                                required: true,
-                                pattern: {
-                                    value: /^(0|[1-9]\d*)(\.\d+)?$/,
-                                    message: "input will number",
-                                },
-                            }
-                        )}
-                    />
-
-
-
-                    {errors?.project_count?.type === "required" && (
-                        <p className="text-red-500">This field is required</p>
-                    )}
-                </div>
-
-
-
-
-
-            </div> */}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            {/* 
-            <input
-                className="bg-blue-600 text-white font-semibold text-center p-3 rounded-md "
-                type="submit"
-            /> */}
-
-
-            <div className="flex">
-                <div
-                    name=" grey window"
-                    className="h-screen  "
-                ><h4 className='font-bold border'>Hello</h4></div>
             </div>
         </div>
+    </div>
     )
 }
 
